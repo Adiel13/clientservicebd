20951027c152fcba6098fb3122463046d9a48fcb
KevinAdiel <201020724@ingenieria.usac.edu.gt>
2016-12-09 16:18:31 -0600

    obtener informacion
    se agrego la funcio nde obtener informacion

--- permisos.sql
+++ permisos.sql
@@ -8,3 +8,10 @@ GRANT EXECUTE ON FUNCTION inicio_sesion(usuario varchar(20), token varchar(32)) 
 GRANT INSERT, UPDATE, DELETE, SELECT ON TABLE sesion TO usrlogin;
 GRANT INSERT, UPDATE, SELECT ON TABLE bitacora_sesion TO usrlogin;
 GRANT EXECUTE ON FUNCTION crear_sesion(usuario varchar(20), sistema_parametro smallint) TO usrlogin;
+
+
+CREATE USER usrconsulta PASSWORD 'c0nsult4';
+GRANT SELECT ON TABLE empleado TO usrconsulta;
+GRANT SELECT ON TABLE empresa TO usrconsulta;
+GRANT SELECT ON TABLE empleado_empresa TO usrconsulta;
+GRANT SELECT ON TABLE usuario TO usrconsulta;
