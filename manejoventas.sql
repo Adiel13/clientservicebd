af1d96dca3f4095b754b622a6de7a95e1efb4318
KevinAdiel <201020724@ingenieria.usac.edu.gt>
2016-12-15 20:47:17 -0600

    Fin venta
    Se crearon las funciones para finalizar una venta.

--- manejoventas.sql
+++ manejoventas.sql
@@ -10,9 +10,24 @@ RETURNS VARCHAR(20) AS $$
         
         IF vendedor IS NOT NULL THEN
             SELECT nextval('id_venta') INTO idventa;    
-            INSERT INTO venta (id_venta, id_empleado, id_empresa, inicio_venta, estado_venta) VALUES(idventa, vendedor, empresa, now(), 1);
+            INSERT INTO venta (id_venta, id_empleado, id_sucursal, inicio_venta, estado_venta) VALUES(idventa, vendedor, empresa, now(), 1);
+            INSERT INTO bitacora_venta (id_venta, id_empleado, id_empresa, inicio_venta, estado_venta) VALUES(idventa, vendedor, empresa, now(), 1);
         ELSE 
             idventa = -1;
         END IF;
 	RETURN idventa;
+END;$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION finalizar_venta(ventaparam smallint)
+RETURNS VARCHAR(20) AS $$
+	DECLARE 
+        numero_filas smallint;
+	BEGIN
+        SELECT count(*) INTO numero_filas FROM venta WHERE id_venta = ventaparam;
+        IF numero_filas = 1 THEN
+            UPDATE venta SET fin_venta = now(), estado_venta = 2 WHERE id_venta = ventaparam;
+            UPDATE bitacora_venta SET fin_venta = now(), estado_venta = 2 WHERE id_venta = ventaparam;
+        END IF;
+	RETURN numero_filas;
 END;$$ LANGUAGE plpgsql;
