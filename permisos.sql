6522168e8db5ed55c2d3c1d9f676002b8f3530fd
Adiel13 <kevinadiel@gmail.com>
2017-03-04 22:17:04 -0600

    Permisos y demás para acciones
    se agregaron usuarios y permisos sobre tablas, procedimientos y
    secuencias.

--- permisos.sql
+++ permisos.sql
@@ -1,5 +1,4 @@
 CREATE USER usrlogin PASSWORD 'l0g1n';
-
 GRANT SELECT ON TABLE usuario TO usrlogin;
 GRANT SELECT ON TABLE tipo_usuario TO usrlogin;
 GRANT SELECT ON TABLE tipo_usuario_permiso TO usrlogin;
@@ -21,6 +20,7 @@ GRANT SELECT ON TABLE sucursal TO usrconsulta;
 GRANT SELECT ON TABLE venta_satisfactoria TO usrconsulta;
 GRANT SELECT ON TABLE bitacora_sesion TO usrconsulta;
 GRANT SELECT ON TABLE estado_venta TO usrconsulta;
+GRANT SELECT ON TABLE tipo_accion TO usrconsulta;
 
 CREATE USER usrventa PASSWORD 'v3nt4';
 GRANT SELECT ON TABLE empleado TO usrventa;
@@ -37,3 +37,14 @@ GRANT EXECUTE ON FUNCTION obtener_empresa(token_sesion varchar(200), sistema_par
 GRANT EXECUTE ON FUNCTION obtenerUsuario(token_sesion varchar(200), sistema_parametro smallint) TO usrventa;
 GRANT ALL ON id_venta TO usrventa;
 GRANT EXECUTE ON  FUNCTION finalizar_venta(ventaparam smallint) TO usrventa;
+
+CREATE USER usraccion PASSWORD '4cci0n';
+GRANT INSERT, UPDATE, DELETE, SELECT ON TABLE accion_empleado TO usraccion;
+GRANT SELECT ON TABLE empleado TO usraccion;
+GRANT SELECT ON TABLE empresa TO usraccion;
+GRANT SELECT ON TABLE empleado_empresa TO usraccion;
+GRANT SELECT ON TABLE usuario TO usraccion;
+GRANT SELECT ON TABLE sesion TO usraccion;
+GRANT ALL ON accion_empleado_id_accion_seq to usraccion;
+GRANT EXECUTE ON  FUNCTION crear_accion(token_sesion varchar(200), mensaje text, tipo smallint, sistemaparametro smallint, vendedor varchar(20)) TO usraccion;
+GRANT EXECUTE ON FUNCTION obtenerUsuario(token_sesion varchar(200), sistema_parametro smallint) TO usraccion;
